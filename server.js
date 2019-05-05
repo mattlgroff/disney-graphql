13dffbc490cdb1ae0e376885c890af04581efd7b
Matthew Groff <mattlgroff@gmail.com>
2019-05-05 11:53:47 -0500

    Enable playground in Production

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { ApolloServer } = require('apollo-server');
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
+  playground: true,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
